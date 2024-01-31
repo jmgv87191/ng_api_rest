96c888e957167ff6b174897af6be609798b32ce2
juan Manuel <jmgv87191@gmail.com>
2024-01-31 11:23:51 -0700

    primer comit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './vistas/login/login.component';
+import { EditarComponent } from './vistas/editar/editar.component';
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+import { NuevoComponent } from './vistas/nuevo/nuevo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component: LoginComponent
+    },
+    {
+        path:'editar',
+        component: EditarComponent
+    },
+    {
+        path:'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path:'nuevo',
+        component: NuevoComponent
+    }
+];
